6094b1841d35d18c87a27041a58aa765df9f4892
NavyaDornala <navyasreed1998@gmail.com>
2018-07-01 13:24:32 +0530

    Connected Luis for query processing

--- AstronomyBot/Apod.cs
+++ AstronomyBot/Apod.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 
 
 namespace AstronomyBot
+
 {
     public class Apod
     {
--- AstronomyBot/Dialogs/RootDialog.cs
+++ AstronomyBot/Dialogs/RootDialog.cs
@@ -4,86 +4,138 @@ using System.Threading.Tasks;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Connector;
 using System.Net;
+using System.Web;
+using System.Xml;
+using System.Text.RegularExpressions;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
 using System.Collections.Generic;
 using Microsoft.Office.Interop.Excel;
 using _Excel = Microsoft.Office.Interop.Excel;
 
 namespace AstronomyBot.Dialogs
 {
+    [LuisModel("6e0e7f8a-3483-41fe-a5ab-a576c6b616d3", "ec622bf0f30f46a6bc27ddd75ecb57f3")]
     [Serializable]
-    public class RootDialog : IDialog<object>
+    public class RootDialog : LuisDialog<object>
     {
-           public Task StartAsync(IDialogContext context)
+        private const string ShowInternetAttachment = "(3) Show Internal attachment";
+        // [LuisIntent("")]
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
         {
-            context.Wait(MessageReceivedAsync);
-            return Task.CompletedTask;
-
-       }
-       
-        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result){
             var activity = await result as Activity;
-            if (activity.Text.Equals("Apod"))
+            if (activity.Text.Equals("help"))
             {
-                var replyMessage = context.MakeMessage();
-                ApodAttributes apod = await Apod.GetDetails();
-                Attachment attachment = await  GetProfileHeroCardAsync(apod.url);
-                replyMessage.Attachments = new List<Attachment> { attachment };
-                await context.PostAsync(replyMessage);
-                context.Wait(MessageReceivedAsync);
-            }
-            else if (activity.Text.Equals("joke"))
-            {
-                string path1 = @"C:\Users\NAVYA\source\repos\AstronomyBot\AstronomyBot\Joke.xlsx";
-                _Application excel = new _Excel.Application();
-                Workbook wb;
-                Worksheet ws;
-                wb = excel.Workbooks.Open(path1);
-                ws = wb.Worksheets[1];
-                Random number1 = new Random();
-                int i = number1.Next(1, 32);
-                if (ws.Cells[i, 1].Value2 != null)
-                {
-                    await context.PostAsync("I've got one for you: ");
-                    await context.PostAsync($"Q : {ws.Cells[i, 1].Value2}\n A : {ws.Cells[i, 2].Value2}");
-                }
-            }
-            else if (activity.Text.Equals("fact"))
-            {
-                string[] lines = System.IO.File.ReadAllLines(@"C:\Users\NAVYA\source\repos\AstronomyBot\AstronomyBot\Facts.txt");
-                Random number = new Random();
-                int num = number.Next(1, 57);
-                await context.PostAsync("Here's something interesting:");
-                await context.PostAsync($"{lines[num]}");
-            }
-            else if (activity.Text.Equals("sunrise"))
-            {
-                currentConditions conditions = await OpenWeather.GetSunAsync();
-                await context.PostAsync(string.Format("Today sunrise Time : {0}", conditions.results.Sunrise));
-                context.Wait(MessageReceivedAsync);
+                await context.PostAsync("Cosmo would tell you:\nAstronomy picture of the day\nFun Facts on astronomy\nJokes on astronomy\nSunrise and sunset timings");
 
-            }
-            else if (activity.Text.Equals("sunset"))
-            {
-                currentConditions conditions = await OpenWeather.GetSunAsync();
-                await context.PostAsync(string.Format("Today's sunset Time : {0}", conditions.results.Sunset));
-                context.Wait(MessageReceivedAsync);
-
-            }
-            else if (activity.Text.Equals("title"))
-            {
-                ApodAttributes apod = await Apod.GetDetails();
-                await context.PostAsync(string.Format("Title : {0}", apod.title));
-                context.Wait(MessageReceivedAsync);
-            }
-            else
-            {
-                currentConditions conditions = await OpenWeather.GetWeatherAsync(activity.Text);
-               // await context.PostAsync(string.Format(conditions.results.Sunrise));
-               await context.PostAsync(string.Format("Current conditions in {1}: {0}. The temperature is {2}\u00B0 C.",conditions.Weather[0].Main,conditions.CityName, (float)(conditions.Main.Temperature - 32) * 5 / 9));
-               context.Wait(MessageReceivedAsync);
             }
         }
-        private static async Task<Attachment> GetProfileHeroCardAsync(string url)
+        [LuisIntent("None")]
+        public async Task NoneIntent(IDialogContext context, LuisResult result)
+        {
+            //            await this.ShowLuisResult(context, result);
+           // await context.PostAsync($"hey enter some thing!!!!");
+            string message = $"Sorry, I did not understand '{result.Query}'. Type 'help' if you need assistance.";
+            await context.PostAsync(message);
+            context.Wait(this.MessageReceived);
+           //await context.PostAsync(message);
+            //context.Wait(this.MessageReceived);
+        }
+        
+        [LuisIntent("Apod.Display")]
+        public async Task GreetingIntent(IDialogContext context, LuisResult result)
+        {
+            var replyMessage = context.MakeMessage();
+            ApodAttributes apod = await Apod.GetDetails();
+            Attachment attachment = await GetProfileHeroCardAsync(apod.url);
+            replyMessage.Attachments = new List<Attachment> { attachment };
+            await context.PostAsync(replyMessage);
+           // context.Wait(MessageReceivedAsync);
+        }
+       /* [LuisIntent("Term.GetWiki")]
+        public async Task WikiIntent(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            var webClient = new WebClient();
+            var pageSourceCode = webClient.DownloadString("http://en.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&exsentences=2&titles=" + activity.Text + "&redirects=true");
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(pageSourceCode);
+            var fnode = doc.GetElementsByTagName("extract")[0];
+
+            try
+            {
+                string ss = fnode.InnerText;
+                Regex regex = new Regex("\\<[^\\>]*\\>");
+                string.Format("Before:{0}", ss);
+                ss = regex.Replace(ss, string.Empty);
+                string res = String.Format(ss);
+                await context.PostAsync(res);
+
+            }
+            catch (Exception)
+            {
+                await context.PostAsync("error");
+            }
+        }*/
+
+        [LuisIntent("Jokes.GetJoke")]
+        public async Task JokeIntent(IDialogContext context, LuisResult result)
+        {
+            string path1 = @"C:\Users\NAVYA\source\repos\AstronomyBot\AstronomyBot\Joke.xlsx";
+            _Application excel = new _Excel.Application();
+            Workbook wb;
+            Worksheet ws;
+            wb = excel.Workbooks.Open(path1);
+            ws = wb.Worksheets[1];
+            Random number1 = new Random();
+            int i = number1.Next(1, 32);
+            if (ws.Cells[i, 1].Value2 != null)
+            {
+                await context.PostAsync("I've got one for you: ");
+                await context.PostAsync($"Q : {ws.Cells[i, 1].Value2}\n A : {ws.Cells[i, 2].Value2}");
+            }
+
+        }
+        [LuisIntent("Facts.GetFact")]
+        public async Task FactIntent(IDialogContext context, LuisResult result)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\NAVYA\source\repos\AstronomyBot\AstronomyBot\Facts.txt");
+            Random number = new Random();
+            int num = number.Next(1, 57);
+            await context.PostAsync("Here's something interesting:");
+            await context.PostAsync($"{lines[num]}");
+        }
+        [LuisIntent("Sunrise.GetTime")]
+        public async Task SunriseIntent(IDialogContext context,LuisResult result)
+        {
+            currentConditions conditions = await OpenWeather.GetSunAsync();
+            await context.PostAsync(string.Format("Today sunrise Time : {0}", conditions.results.Sunrise));
+           // context.Wait(MessageReceivedAsync);
+        }
+         [LuisIntent("Sunset.GetTime")]
+        public async Task SunsetIntent(IDialogContext context, LuisResult result)
+        {
+             currentConditions conditions = await OpenWeather.GetSunAsync();
+             await context.PostAsync(string.Format("Today's sunset Time : {0}", conditions.results.Sunset));
+             //context.Wait(MessageReceivedAsync);
+
+         }
+        /* else if (activity.Text.Equals("title"))
+         {
+             ApodAttributes apod = await Apod.GetDetails();
+             await context.PostAsync(string.Format("Title : {0}", apod.title));
+             context.Wait(MessageReceivedAsync);
+         }
+         else
+         {
+             currentConditions conditions = await OpenWeather.GetWeatherAsync(activity.Text);
+            // await context.PostAsync(string.Format(conditions.results.Sunrise));
+            await context.PostAsync(string.Format("Current conditions in {1}: {0}. The temperature is {2}\u00B0 C.",conditions.Weather[0].Main,conditions.CityName, (float)(conditions.Main.Temperature - 32) * 5 / 9));
+            context.Wait(MessageReceivedAsync);
+         }*/
+
+        public async Task<Attachment> GetProfileHeroCardAsync(string url)
         {
             ApodAttributes apod = await Apod.GetDetails();
             var heroCard = new HeroCard
@@ -91,10 +143,11 @@ namespace AstronomyBot.Dialogs
                 Title = string.Format(apod.title),
                 Subtitle = string.Format(apod.Explanation),
                 Tap = new CardAction(ActionTypes.OpenUrl, "Learn More", value: "http://antwrp.gsfc.nasa.gov/apod/astropix.html"),
-                Images = new List<CardImage> { new CardImage(apod.url) }, 
-               // Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "http://www.devenvexe.com"), new CardAction(ActionTypes.OpenUrl, "C# Corner", value: "http://www.c-sharpcorner.com/members/suthahar-j"), new CardAction(ActionTypes.OpenUrl, "MSDN", value: "https://social.msdn.microsoft.com/profile/j%20suthahar/") }
+                Images = new List<CardImage> { new CardImage(apod.url) },
+                // Buttons = new List<CardAction> { new CardAction(ActionTypes.OpenUrl, "Learn More", value: "http://www.devenvexe.com"), new CardAction(ActionTypes.OpenUrl, "C# Corner", value: "http://www.c-sharpcorner.com/members/suthahar-j"), new CardAction(ActionTypes.OpenUrl, "MSDN", value: "https://social.msdn.microsoft.com/profile/j%20suthahar/") }
             };
             return heroCard.ToAttachment();
+
         }
     }
 }
